e8770b8bd64d0b9c0a72766eb8e8a88b09df2f1b
saroj7 <sarojsht377@gmail.com>
2021-04-06 13:15:45 +0545

    Templates

--- flask_blog.py
+++ flask_blog.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Saroj Shrestha',
+        'title': 'Blog Post 1',
+        'content': 'First Blog Content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Sanju Shrestha',
+        'title': 'Blog Post 2',
+        'content': 'Second Blog Content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html', posts = posts)
 
 
 @app.route('/about')
 def about():
-    return '<h1>About PAge</h1>'
+    return render_template('about.html', title = 'About')
 
 
 if __name__ == '__main__':
